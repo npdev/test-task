37b141107286b03fc1e77732eff64828ac0bc846
npdev <sashko.dev@gmail.com>
2013-03-03 18:52:02 +0200

    using widget in app

--- demos/blog/protected/config/main.php
+++ demos/blog/protected/config/main.php
@@ -11,7 +11,7 @@ return array(
 
 	// preloading 'log' component
 	'preload'=>array('log'),
-
+				'modules'=>array('chat'),
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
--- demos/blog/protected/views/layouts/main.php
+++ demos/blog/protected/views/layouts/main.php
@@ -42,7 +42,7 @@
 	)); ?><!-- breadcrumbs -->
 
 	<?php echo $content; ?>
-
+	<?php $this->widget('chat.components.ChatWidget'); ?>
 	<div id="footer">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
